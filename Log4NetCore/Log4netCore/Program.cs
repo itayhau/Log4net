2dbee5696b6e81d0a5867d208d228f0f8d8a39ec
itayhau <itayhau@gmail.com>
2020-11-25 07:47:31 +0200

    Add files via upload

---
+++ Log4NetCore/Log4netCore/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Log4NetCore;
+using log4net;
+using log4net.Config;
+using System.Reflection;
+using System.IO;
+
+namespace Log4NetCore
+{
+
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                var repository = log4net.LogManager.GetRepository(Assembly.GetCallingAssembly());
+
+                var fileInfo = new FileInfo(@"log4net.config");
+
+                log4net.Config.XmlConfigurator.Configure(repository, fileInfo);
+
+                log.Info("test");
+
+                Console.WriteLine("press any key");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
